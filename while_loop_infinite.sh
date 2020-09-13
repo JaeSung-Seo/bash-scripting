35b3d79e0db6d51acd07dd1749d115df10d75c46
JaeSung-Seo <hotshotseo@gmail.com>
2020-09-12 20:54:03 -0400

    Update and rename while_loo_infinite.sh to while_loop_infinite.sh

--- while_loop_infinite.sh
+++ while_loop_infinite.sh
@@ -4,8 +4,9 @@ count = 0
 
 while true;
 do
-  time curl -o /dev/null www.google.com
+  time curl -o /dev/null $1
   count=$(expr $count + 1 )
+  echo " Target url: $1"
   echo " Attempts: $count"
   sleep 2
 done
